54bee07a1d84b978c213ff8924ce055ccbfb49c6
Javier Elio <javiereliomedina@gmail.com>
2021-04-13 14:59:56 +0200

    Add ggplot theme on _common.R

--- _common.R
+++ _common.R
@@ -27,3 +27,20 @@ Sys.setenv(UDUNITS2_XML_PATH="")
 if (knitr::is_latex_output())
   options(width = 66)
 #options(width = 72)
+
+
+# Create our own theme for plotting maps in ggplot2.
+  my_theme_map <- function() {
+    theme_void() +
+      theme(
+        # Title and captions
+        plot.title = element_text(size = 14, colour = "darkblue", face = "bold"),
+        plot.caption = element_text(size = 10, colour = "grey25"),
+        # Legend
+        legend.title = element_text(size = 10, face = "bold"),
+        legend.text = element_text(size = 10),
+        # Facets
+        strip.text = element_text(size = 12, color = "black", face = "italic")
+      )
+  }
+
